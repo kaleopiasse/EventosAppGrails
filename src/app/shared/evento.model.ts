f439cb5668ac1bfe1afbd8aa68a1a7af69ec1e40
Kaleo Piasse <kaleo.piasse@gmail.com>
2018-04-24 22:08:06 -0300

    Campo date

--- src/app/shared/evento.model.ts
+++ src/app/shared/evento.model.ts
@@ -2,7 +2,7 @@ export class Evento{
     public id: number
     constructor(
     public titulo: string,
-    public data: string,
+    public data: Date,
     public horaInicio: any,
     public horaFim: any,
     public descricao: string
