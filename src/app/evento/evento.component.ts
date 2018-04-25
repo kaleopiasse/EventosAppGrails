2fce2277aea996665d4016e48e1d8248cbf38836
Kaleo Piasse <kaleopiasse@gmail.com>
2018-04-25 18:28:27 -0300

    Versão com verificacao data e hora, mas com alguns erros

--- src/app/evento/evento.component.ts
+++ src/app/evento/evento.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms'
-import {Evento} from '../shared/evento.model'
-import {EventoService} from '../evento.service'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { Evento } from '../shared/evento.model'
+import { EventoService } from '../evento.service'
 import { ActivatedRoute } from '@angular/router';
-import {Observable} from 'rxjs/Observable'
-import {Router} from '@angular/router';
+import { Observable } from 'rxjs/Observable'
+import { Router } from '@angular/router';
+import { parse } from 'path';
 
 @Component({
   selector: 'app-evento',
@@ -17,23 +18,23 @@ export class EventoComponent implements OnInit {
   //public idEventoSalvo : number
   public evento: Evento[]
   public option: string = 'false'
-  public id : string
-  public mensagem : boolean = false
-  public msg : string = 'teste'
+  public id: string
+  public mensagem: boolean = false
+  public msg: string = ''
 
   public formulario: FormGroup = new FormGroup({
-    'titulo': new FormControl(null,[Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
-    'data': new FormControl(null,[Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
-    'horaInicio': new FormControl(null,[Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
-    'horaFim': new FormControl(null,[Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
-    'descricao': new FormControl(null,[Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'titulo': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'data': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'horaInicio': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'horaFim': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'descricao': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
   })
 
-  constructor(private eventoService: EventoService, private route: ActivatedRoute, private router: Router) { 
+  constructor(private eventoService: EventoService, private route: ActivatedRoute, private router: Router) {
     this.option = this.route.snapshot.params['option']
     this.id = this.route.snapshot.params['id']
     console.log(this.option, this.id)
-    if(this.option==='true'){
+    if (this.option === 'true') {
       this.preencherEvento()
     }
   }
@@ -42,14 +43,15 @@ export class EventoComponent implements OnInit {
   }
 
   public salvarEvento(): void {
-    if(this.formulario.status === 'INVALID'){
+    if (this.formulario.status === 'INVALID') {
       this.formulario.get('titulo').markAsTouched()
       this.formulario.get('data').markAsTouched()
       this.formulario.get('horaInicio').markAsTouched()
       this.formulario.get('horaFim').markAsTouched()
       this.formulario.get('descricao').markAsTouched()
+      console.log(this.verificaData())
     }
-    else{
+    else {
       let evento: Evento = new Evento(
         this.formulario.value.titulo,
         this.formulario.value.data,
@@ -59,17 +61,17 @@ export class EventoComponent implements OnInit {
       )
 
       evento.data = (new Intl.DateTimeFormat('pt-BR').format(new Date(evento.data)))
-      console.log(evento)
-      
+      console.log(this.verificaData())
+
       this.eventoService.postEvento(evento)
-      .subscribe((idEvento: number) => {
-        console.log(idEvento)
-        this.feedbackUser("Evento salvo com sucesso !!!")
-      })
+        .subscribe((idEvento: number) => {
+          console.log(idEvento)
+          this.feedbackUser("Evento salvo com sucesso !!!")
+        })
     }
   }
 
-  public atualizarEvento(){
+  public atualizarEvento() {
     let evento: Evento = new Evento(
       this.formulario.value.titulo,
       this.formulario.value.data,
@@ -77,57 +79,86 @@ export class EventoComponent implements OnInit {
       this.formulario.value.horaFim,
       this.formulario.value.descricao
     )
-    evento.id=parseInt(this.id)
+    evento.id = parseInt(this.id)
+    evento.data = (new Intl.DateTimeFormat('pt-BR').format(new Date(evento.data)))
     console.log("Atualizar evento")
     console.log(evento)
 
     this.eventoService.updateEvento(evento)
-    .subscribe((idEvento: number) => {
-      console.log(idEvento)
-      this.feedbackUser("Evento atualizado com sucesso !!!")
-    })
+      .subscribe((idEvento: number) => {
+        console.log(idEvento)
+        this.feedbackUser("Evento atualizado com sucesso !!!")
+      })
   }
 
-  public preencherEvento(){
+  public preencherEvento() {
     console.log("Preenchendo dados evento")
     this.eventoService.getEventoById(this.id)
-    .subscribe((evento: Evento[]) =>{
-      this.evento = evento
-      this.formulario.setValue({
-        titulo:evento['titulo'], 
-        data:evento['data'], 
-        horaInicio:evento['horaInicio'], 
-        horaFim: evento['horaFim'], 
-        descricao:evento['descricao']})
-    })
+      .subscribe((evento: Evento[]) => {
+        this.evento = evento
+        this.formulario.setValue({
+          titulo: evento['titulo'],
+          data: evento['data'],
+          horaInicio: evento['horaInicio'],
+          horaFim: evento['horaFim'],
+          descricao: evento['descricao']
+        })
+      })
   }
 
-  public deletarEvento(){
+  public deletarEvento() {
     this.eventoService.deleteEventoById(this.id)
-    .subscribe((code: number) => {
-      console.log(code)
-      this.feedbackUser("Evento deletado com sucesso !!!")
+      .subscribe((code: number) => {
+        console.log(code)
+        this.feedbackUser("Evento deletado com sucesso !!!")
+      })
+  }
+
+  public limparForm() {
+    this.formulario.setValue({
+      titulo: [''],
+      data: [''],
+      horaInicio: [''],
+      horaFim: [''],
+      descricao: ['']
     })
   }
 
-  public limparForm(){
-    this.formulario.setValue({
-      titulo:[''], 
-      data:[''], 
-      horaInicio:[''], 
-      horaFim:[''], 
-      descricao:['']})
-  }
-
-  public feedbackUser(msg:string){
+  public feedbackUser(msg: string) {
     this.msg = msg
-    this.mensagem=true
-    setTimeout(()=>(this.router.navigate(['/home'])),2000)
+    this.mensagem = true
+    setTimeout(() => (this.router.navigate(['/home'])), 2000)
     this.limparForm()
     this.formulario.get('titulo').markAsUntouched()
     this.formulario.get('data').markAsUntouched()
     this.formulario.get('horaInicio').markAsUntouched()
     this.formulario.get('horaFim').markAsUntouched()
     this.formulario.get('descricao').markAsUntouched()
+  }
+
+  public verificaData() {
+    var data_atual = new Date()
+    var diaDoMes = data_atual.getDate()
+    data_atual.setDate(diaDoMes - 1)
+    var data_usuario = new Date(this.formulario.value.data)
+
+    if (data_usuario < data_atual) {
+      this.formulario.get('data').invalid
+      return false
+    }
+    return true
+  }
+
+  public verificaHora() {
+    var horaInicio = this.formulario.value.horaInicio
+    var horaFim = this.formulario.value.horaFim
+
+    if (horaInicio != null && horaFim != null) {
+      if (horaInicio > horaFim) {
+        this.formulario.get('horaFim').invalid
+        return false
+      }
+    }
+    return true
   }
 }
